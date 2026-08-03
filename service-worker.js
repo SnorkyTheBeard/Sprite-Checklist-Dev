1ec56a964318d01ad3d140f80f416e721ead4a4a
SnorkyTheBeard <aaron.taylor.jones@gmail.com>
2026-08-03 11:36:43 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE = 'galaxy-sprite-checklist-v111';
+const CACHE = 'galaxy-sprite-checklist-v112';
 const CORE = [
   './',
   './index.html',
   './styles.css?v=110',
-  './vault-v105.css?v=111',
+  './vault-v105.css?v=112',
   './viewport-v106.css?v=107',
   './published-design.js',
   './art-config.js?v=67',
   './data.js?v=82',
-  './app.js?v=111',
+  './app.js?v=112',
   './manifest.webmanifest?v=101',
   './fonts/Burbank Big Condensed Black.otf',
   './fonts/Fredoka-Regular.woff2',
