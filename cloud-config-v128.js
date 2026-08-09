c617eea26b97c44ec33ba6c0a29dd63dc1d27616
SnorkyTheBeard <aaron.taylor.jones@gmail.com>
2026-08-09 00:04:25 -0500

    Update cloud-config-v128.js

--- cloud-config-v128.js
+++ cloud-config-v128.js
@@ -4,7 +4,7 @@
   This browser key is safe to publish only when Row Level Security is enabled.
 */
 window.SPRITE_CLOUD_CONFIG = Object.freeze({
-  supabaseUrl:'',
-  supabaseAnonKey:'',
+  supabaseUrl:'https://nwvgccvlqqhcqvjtqyqj.supabase.co',
+  supabaseAnonKey:'sb_publishable_q3fYdAlX7b1ct3vrCpEk-Q_rHy45Lx7',
   saveTable:'sprite_tracker_saves'
 });
